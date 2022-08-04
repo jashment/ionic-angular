71af26f8b2429ad09ca4e784164a2d4a7c784c86
Jordan <jordanashment@gmail.com>
2022-08-04 09:39:07 -0600

    Building out new Ionic-Angular monorepo

---
+++ apps/my-app/src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { ReusableModalComponent } from 'libs/ui-components/reusable-modal/reusable-modal.component';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, ReusableModalComponent],
+})
+export class HomePageModule { }
---
+++ libs/ui-components/reusable-modal/reusable-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ia-app-reusable-modal',
+  templateUrl: './reusable-modal.component.html',
+  styleUrls: ['./reusable-modal.component.scss']
+})
+export class ReusableModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
